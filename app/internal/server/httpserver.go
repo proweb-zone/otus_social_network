63a7b4dff54f113b8be65345c767e1f8c987762a
ALexandr Proskuryakov <proskuryakov.a@toledo24.ru>
2025-05-16 20:04:35 +0300

    add middlware check token

--- app/internal/server/httpserver.go
+++ app/internal/server/httpserver.go
@@ -3,10 +3,10 @@ package server
 import (
 	"net/http"
 	"otus_social_network/app/internal/app/handlers"
+	"otus_social_network/app/internal/app/middleware"
 	"otus_social_network/app/internal/config"
 
 	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
 	_ "github.com/lib/pq"
 )
 
@@ -14,9 +14,8 @@ func StartServer(config *config.Config) {
 	handlers := handlers.Init(config)
 
 	r := chi.NewRouter()
-	r.Use(middleware.Logger)
 	r.Post("/login", handlers.Login)
 	r.Post("/user/register", handlers.Register)
-	r.Get("/user/get/{id}", handlers.GetUser)
+	r.With(middleware.CheckAccess(config)).Get("/user/get/{id}", handlers.GetUser)
 	http.ListenAndServe(":"+config.HTTPServer.ServerPort, r)
 }
